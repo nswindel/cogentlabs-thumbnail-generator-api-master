3084f4384136870ea06a099ca1bc371f9f998668
nswindel <n_swindells@hotmail.com>
2025-05-06 16:12:03 +0900

    Initial testing of exposing /metrics /health route for api.
    Build new image and cache locally:
    ```
    $ npm install ; npm run build ; npm run test
    eval $(minikube docker-env)
    docker build -t thumbnail-api .
    ```
    
    Deploy new api and curl route via api service:
    ```
    $ curl http://10.109.17.208:3000/metrics
    <snip>
    nodejs_gc_duration_seconds_bucket{le="5",kind="incremental"} 2
    nodejs_gc_duration_seconds_bucket{le="+Inf",kind="incremental"} 2
    nodejs_gc_duration_seconds_sum{kind="incremental"} 0.02067529201507568
    nodejs_gc_duration_seconds_count{kind="incremental"} 2
    $
    
    $ curl http://10.109.17.208:3000/health
    {"data":"OK"}$ exit
    $
    ```

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,28 @@ app.use(express.json({ limit: '20mb' }));
 
 const port = 3000;
 
+const client = require('prom-client');
+
+client.collectDefaultMetrics();
+const register = client.register;
+
+// Metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
 app.get('/', (_: Request, res: Response) => {
   res.status(200).send({ data: 'Hello from Thumbnail Generator' });
 });
 
+// Health check endpoint
+app.get('/health', async (req, res: Response) => {
+  res.status(200).send({ data: 'OK' });
+});
+
 // Thumbnail controller
 app.use('/thumbnail', thumbnailRoutes);
+
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
