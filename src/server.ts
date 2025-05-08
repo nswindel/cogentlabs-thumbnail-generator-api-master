b5cdaba2a7886b671530f4b491afa7113fc6fcdf
nswindel <n_swindells@hotmail.com>
2025-05-08 22:32:58 +0900

    Add counters for requests and http responses
    Testing:
    ```
    helm uninstall thumbnail-app
    npm install
    npm run build
    npm run test
    docker build -t thumbnail-api:v1.0.0 .
    docker build -t thumbnail-task:v1.0.0 .
    helm install thumbnail-app ./thumbnail-app
    ```
    
    Checked the api:3000/metrics post helm deployment
    ```
    http_request_duration_milliseconds_bucket{le="500",method="POST",route="/thumbnail/",code="200"} 24
    http_request_duration_milliseconds_bucket{le="1000",method="POST",route="/thumbnail/",code="200"} 24
    http_request_duration_milliseconds_bucket{le="+Inf",method="POST",route="/thumbnail/",code="200"} 24
    http_request_duration_milliseconds_sum{method="POST",route="/thumbnail/",code="200"} 0.15265348699999998
    http_request_duration_milliseconds_count{method="POST",route="/thumbnail/",code="200"} 24
    
    http_requests_total{method="GET",route="/thumbnail/:id"} 19
    http_requests_total{method="POST",route="/thumbnail/"} 24
    ```

--- src/server.ts
+++ src/server.ts
@@ -13,22 +13,33 @@ client.collectDefaultMetrics();
 const register = client.register;
 
 const httpRequestDurationMilliseconds = new client.Histogram({
-  name: 'node_request_duration_milliseconds',
+  name: 'http_request_duration_milliseconds',
   help: 'Duration of HTTP requests in milliseconds',
   labelNames: ['method', 'route', 'code'],
   buckets: [50, 100, 200, 300, 400, 500, 1000],
 });
 
-app.use((req, res, next) => {
-  const start = Date.now();
-  res.on('finish', () => {
-    const responseTimeInMilliseconds = Date.now() - start;
-    httpRequestDurationMilliseconds
-      .labels(req.method, req.route?.path)
-      .observe(responseTimeInMilliseconds);
-  });
-  next()
+const totalHttpRequests = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route'],
 });
+
+const non200Responses = new client.Counter({
+  name: 'http_requests_non_200_total',
+  help: 'Total number of HTTP responses with status codes other than 200',
+  labelNames: ['method', 'route', 'code'],
+});
+
+function withMetrics(routePath: string, handler: (req: Request, res: Response) => void) {
+  return (req: Request, res: Response) => {
+    const end = httpRequestDurationMilliseconds.startTimer();
+    res.on('finish', () => {
+      end({ method: req.method, route: routePath, code: res.statusCode.toString() });
+    });
+    handler(req, res);
+  };
+}
 
 // Metrics endpoint
 app.get('/metrics', async (req, res) => {
@@ -36,17 +47,29 @@ app.get('/metrics', async (req, res) => {
   res.end(await register.metrics());
 });
 
-app.get('/', (_: Request, res: Response) => {
+app.get('/', withMetrics('/', (_: Request, res: Response) => {
   res.status(200).send({ data: 'Hello from Thumbnail Generator' });
-});
+}));
 
 // Health check endpoint
-app.get('/health', async (req, res: Response) => {
+app.get('/health', withMetrics('/health', async (_req, res) => {
   res.status(200).send({ data: 'OK' });
-});
+}));
 
 // Thumbnail controller
-app.use('/thumbnail', thumbnailRoutes);
-
+app.use('/thumbnail', (req, res, next) => {
+  const end = httpRequestDurationMilliseconds.startTimer();
+  res.on('finish', () => {
+    const routePath = `/thumbnail${req.route?.path || ''}`;
+    end({ method: req.method, route: routePath, code: res.statusCode.toString() });
+    // Increment the total HTTP requests counter
+    totalHttpRequests.inc({ method: req.method, route: routePath });
+    // Increment the non-200 responses counter if the status code is not 200
+    if (res.statusCode !== 200) {
+      non200Responses.inc({ method: req.method, route: routePath, code: res.statusCode.toString() });
+    }
+  });
+  next();
+}, thumbnailRoutes);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
